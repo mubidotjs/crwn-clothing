42cf1e04e0c853ea64c675161937234eee7a0f95
mubihussain <mubikhan69@gmail.com>
2020-08-22 14:35:08 +0500

    updating app with a shop page, collection preview component and collection item component. Also updated cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,18 @@
+// Utility functions allow us to keep our files clean and organize functions that we
+// may need in multiple files in one location i.e. same items in array in this example.
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
